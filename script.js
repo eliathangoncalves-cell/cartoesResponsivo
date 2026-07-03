ba2601ca5c0b138c4e1e6c0f46e75e541a46f0a2
eliathangoncalves-cell <eliathan.goncalves@escola.pr.gov.br>
2026-07-03 12:47:28 +0000

    iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

---
+++ script.js
@@ -0,0 +1,31 @@
+const botao1 = document.getElementById("botao1") 
+const botao2 = document.getElementById("botao2")
+const botao3 = document.getElementById("botao3")
+const botao4 = document.getElementById("botao4")
+const botao5 = document.getElementById("botao5")
+
+
+
+botao1.addEventListener('click',() =>{
+alert("aaaaaaaaaaaaaaa");
+});
+
+
+botao2.addEventListener('click',() =>{
+alert("aaaaaaaaaaaaaaa");
+});
+
+
+botao3.addEventListener('click',() =>{
+alert("aaaaaaaaaaaaaaa");
+});
+
+
+botao4.addEventListener('click',() =>{
+alert("aaaaaaaaaaaaaaa");
+});
+
+
+botao5.addEventListener('click',() =>{
+alert("aaaaaaaaaaaaaaa");
+});
